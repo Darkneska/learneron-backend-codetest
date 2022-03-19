e8e574aafd9a8d11d9e5dfeac60539f6521359c4
Darkneska <84958830+Darkneska@users.noreply.github.com>
2022-03-19 09:26:34 +0100

    django models, many-to-many, djangoobjecttype, query to get all movies

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -1,8 +1,57 @@
 import graphene
+from graphene_django import DjangoObjectType
+from .models import Movie, Actor
+
+
+class MovieType(DjangoObjectType):
+    class Meta:
+        model = Movie
+        fields = ('id', 'name', 'year')
+
+
+class ActorType(DjangoObjectType):
+    class Meta:
+        model = Actor
+        fields = ('id', 'name', 'date_of_birth')
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    movies = graphene.List(MovieType)
+    actors = graphene.List(ActorType)
+
+    def resolve_movies(self, info, **kwargs):
+        return Movie.objects.all()
+
+    def resolve_actors(self, info, **kwargs):
+        return Actor.objects.all()
 
 
+#
+# class ActorInput(graphene.InputObjectType):
+#     id = graphene.ID()
+#     name = graphene.String()
+#     date_of_birth = graphene.String()
+#     title = graphene.String()
+#
+#
+# class CreateActor(graphene.Mutation):
+#     class Arguments:
+#         actor_data = ActorInput(required=True)
+#
+#     actors = graphene.Field(ActorType)
+#
+#     @staticmethod
+#     def mutate(root, info, actor_data=None):
+#         actor_instance = Actor(
+#             name=actor_data.name,
+#             date_of_birth=actor_data.year,
+#         )
+#         actor_instance.save()
+#         return CreateActor(actor=actor_instance)
+#
+#
+# class Mutation(graphene.ObjectType):
+#     create_actor = CreateActor.Field()
+#
+#
 schema = graphene.Schema(query=Query)
