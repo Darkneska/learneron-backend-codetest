5c8c619baf680b9af4e95c5527ef0f30bb90f83d
Darkneska <84958830+Darkneska@users.noreply.github.com>
2022-03-20 18:50:10 +0100

    fix schema.py

--- graphql_api/schema.py
+++ graphql_api/schema.py
@@ -12,46 +12,16 @@ class MovieType(DjangoObjectType):
 class ActorType(DjangoObjectType):
     class Meta:
         model = Actor
-        fields = ('id', 'name', 'date_of_birth')
+        fields = ('id', 'name', 'date_of_birth', 'title')
 
 
 class Query(graphene.ObjectType):
     movies = graphene.List(MovieType)
-    actors = graphene.List(ActorType)
 
-    def resolve_movies(self, info, **kwargs):
+    def resolve_movies(root, info):
+        # Querying a list
         return Movie.objects.all()
 
-    def resolve_actors(self, info, **kwargs):
-        return Actor.objects.all()
 
-
-#
-# class ActorInput(graphene.InputObjectType):
-#     id = graphene.ID()
-#     name = graphene.String()
-#     date_of_birth = graphene.String()
-#     title = graphene.String()
-#
-#
-# class CreateActor(graphene.Mutation):
-#     class Arguments:
-#         actor_data = ActorInput(required=True)
-#
-#     actors = graphene.Field(ActorType)
-#
-#     @staticmethod
-#     def mutate(root, info, actor_data=None):
-#         actor_instance = Actor(
-#             name=actor_data.name,
-#             date_of_birth=actor_data.year,
-#         )
-#         actor_instance.save()
-#         return CreateActor(actor=actor_instance)
-#
-#
-# class Mutation(graphene.ObjectType):
-#     create_actor = CreateActor.Field()
-#
-#
 schema = graphene.Schema(query=Query)
+
